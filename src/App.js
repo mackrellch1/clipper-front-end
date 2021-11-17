2b08d145c79ff4fe7b60ec8236e22a17d44c4b5c
cmackrell <colin.mackrell99@gmail.com>
2021-11-16 22:35:25 -0500

    Co-authored-by: Torrey Leonard <torreyleonard@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	
+	const [personTalking, setPersonTalking] = useState(null);
+
+	const getSoundUrl = (id) => `https://storage.googleapis.com/discord-clips/${id}.ogg`
+	const getSound = async () => {
+		//const [response] = await fetch("http://localhost:2002/random", {
+		const [response] = await fetch("https://discord-clips-api-5vhmvf6quq-uk.a.run.app/random", {
+			method: "GET",
+			headers: {
+				'Content-Type':'application/json'
+			},
+		}).then(r => r.json());
+		setPersonTalking(response.userName);
+		const audio = new Audio(getSoundUrl(response._id));
+		audio.play();
+		audio.onended = () => setPersonTalking(null);
+		console.log(response);
+	}
+
+	return (
+		<div className="App">
+		<header className="App-header">
+			<img src={logo} className="App-logo" alt="logo" />
+			<p>
+				{personTalking && (
+					<span>
+						Speaking: {personTalking}
+					</span>
+				)}
+			</p>
+			<span
+				className="App-link"
+				onClick={getSound}
+			>
+				Play Sound
+			</span>
+		</header>
+		</div>
+	);
 }
 
 export default App;
